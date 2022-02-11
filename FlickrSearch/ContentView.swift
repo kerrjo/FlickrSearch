65f5f40144885de772af8acd988ae1bab42f0ab6
Joe <kerrjo@me.com>
2022-02-11 09:35:27 -0500

    Initial Commit

---
+++ FlickrSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlickrSearch
+//
+//  Created by JOSEPH KERR on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlickrSearch/FlickrSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchApp.swift
+//  FlickrSearch
+//
+//  Created by JOSEPH KERR on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
