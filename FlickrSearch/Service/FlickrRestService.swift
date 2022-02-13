10d6c479aac3a7b240124212a3b885ebfeb5bb66
Joe <kerrjo@me.com>
2022-02-13 12:43:13 -0500

    removes unused service handler

--- FlickrSearch/Service/FlickrRestService.swift
+++ FlickrSearch/Service/FlickrRestService.swift
@@ -144,93 +144,11 @@ extension FlickrNetworkWebServiceHandler {
 }
 
 
-// MARK: some ServiceHandler not FlickrWebService
-
-/**
- A Combine publisher for Flikr request
- */
-class FlickrNetworkServiceHandler {
-    private(set) var dataTask: URLSessionDataTask?
-    
-    func flickrServiceURL(searchTerm: String) -> URL? {
-        guard var components = URLComponents(string: "https://api.flickr.com/services/feeds/photos_public.gne") else { return nil }
-        components.queryItems = [
-            URLQueryItem(name: "format", value: "json"),
-            URLQueryItem(name: "nojsoncallback", value: "1"),
-            URLQueryItem(name: "tags", value: searchTerm)
-        ]
-        return components.url
-    }
-    
-    private var cancellable: AnyCancellable?
-    
-    func fetchPhotos(searchTerm: String, completion: @escaping (Result<Flickr, FetchError>) -> ()) {
-        guard let url = flickrServiceURL(searchTerm: searchTerm) else { return completion(.failure(.malformedURL)) }
-        print(url)
-        
-        cancellable = fetchUsinDataNetworkService(url)
-        //        cancellable = fetchUsinNetworkService(url)
-            .sink(receiveCompletion: {
-                switch $0 {
-                case .failure(let error):
-                    completion(.failure(.error(error)))
-                case .finished:
-                    break
-                }
-                print ("Received completion: \($0).")
-            },
-                  receiveValue: { response in
-                print ("Received user: \(response).")
-                completion(.success(response))
-                
-            })
-    }
-    
-    private func fetch(_ url: URL) -> AnyPublisher<Flickr, Error> {
-        print(url)
-        return URLSession.shared.dataTaskPublisher(for: url)
-            .tryMap() { element -> Data in
-                guard let httpResponse = element.response as? HTTPURLResponse,
-                      httpResponse.statusCode == 200 else {
-                          throw URLError(.badServerResponse)
-                      }
-                return element.data
-            }
-            .decode(type: Flickr.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
-    }
-    
-    /// uses general handler
-    
-    private func fetchUsinNetworkService(_ url: URL) -> AnyPublisher<Flickr, NetworkService.FailureReason> {
-        print(url)
-        return NetworkService.request(url: url)
-    }
-    
-    /// uses Sample service general handler
-    
-    private func fetchUsinDataNetworkService(_ url: URL) -> AnyPublisher<Flickr, Error> {
-        print(url)
-        return fetchDataUsinDataNetworkService(url)
-            .decode(type: Flickr.self, decoder: JSONDecoder())
-            .eraseToAnyPublisher()
-    }
-    
-    private func fetchDataUsinDataNetworkService(_ url: URL) -> AnyPublisher<Data, DataNetworkService.APIError> {
-        print(url)
-        return dataService.fetch(url: url)
-    }
-    
-    private let dataService = DataNetworkService()
-}
-
-
 // MARK: sample retry service ServiceHandler not FlickrWebService
 
 /**
  Retry Transient Errors and Catch and Replace Persistent Errors
  Any app that uses the network should expect to encounter errors, and your app should handle them gracefully. Because transient network errors are fairly common, you may want to immediately retry a failed data task.
- 
  */
 class RetryNetworkServiceHandler {
     private var cancellable: AnyCancellable?
@@ -253,4 +171,3 @@ class RetryNetworkServiceHandler {
             })
     }
 }
-
